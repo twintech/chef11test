c19030ba39494dd05b8df27768e19fb761b549a9
Marius Popa <marius.popa@twintech.ro>
2013-06-21 12:57:27 +0300

    init script

--- init.sh
+++ init.sh
@@ -3,3 +3,5 @@ knife data bag from file groups data_bags/groups/*.json
 knife data bag create users
 knife data bag from file users data_bags/users/*.json
 knife role from file chef-server.rb
+
+knife cookbook upload -aV
