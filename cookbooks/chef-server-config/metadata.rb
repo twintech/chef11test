3d398fb9792386ee5f76f5d830d5cc3914b63288
Marius Popa <marius.popa@twintech.ro>
2013-06-22 14:48:03 +0300

    chef-server users

--- cookbooks/chef-server-config/metadata.rb
+++
@@ -1,7 +0,0 @@
-name             'chef-server-config'
-maintainer       'YOUR_COMPANY_NAME'
-maintainer_email 'YOUR_EMAIL'
-license          'All rights reserved'
-description      'Installs/Configures chef-server-config'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
