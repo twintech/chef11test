23bad8aedec3fdd8b2793a024b0293619f0de369
Marius Popa <marius.popa@twintech.ro>
2013-06-22 14:42:34 +0300

    chef-server users

--- cookbooks/chef-server-config/recipes/chef-users.rb
+++ cookbooks/chef-server-config/recipes/chef-users.rb
@@ -11,7 +11,7 @@ if users.nil? || users.empty?
 end
 
 users.each do |u|
-  chef-server-config_chefuser "user" do
+  chefserverconfig-chefuser "lalala" do
     user u['user']
     password u['password']
     admin u['admin']
