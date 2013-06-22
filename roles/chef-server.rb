123ea9a6db1815585fd200759467874498725093
Marius Popa <marius.popa@twintech.ro>
2013-06-22 15:43:16 +0300

    chef-server users

--- roles/chef-server.rb
+++ roles/chef-server.rb
@@ -1,9 +1,6 @@
 name "chef-server"
 description "CHEF Server"
-recipes 
-  "role[dev]",
-  "recipe[ntp]",
-  "recipe[timezone]",
+recipes "role[dev]",
   "recipe[chefserverconfig::chef-users]"
       
 override_attributes(
